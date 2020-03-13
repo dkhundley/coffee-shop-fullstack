3f465db8fe348e15371a9310b85f221ec9a06ae2
David Hundley <david.k.hundley@gmail.com>
2020-03-13 10:25:41 -0500

    Updating front end config

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,3 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
@@ -9,6 +5,6 @@ export const environment = {
     url: 'dkhundley', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'YGdlsP3nGYP53AO4yINKytqrvjlv6isy', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application.
   }
 };
